882e631c94424e21200b8d994d34d29132575ffc
CodeWard <mahmoud.elshony43@gmail.com>
2024-10-15 08:48:51 -0700

    angular lap

--- src/app/lab/hero service/hero.lab.service.integration.spec.ts
+++ src/app/lab/hero service/hero.lab.service.integration.spec.ts
@@ -1,5 +1,62 @@
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { HeroServiceForLab } from "./hero.lab.service";
+import { provideHttpClient } from '@angular/common/http';
+import { Hero } from '../../hero';
+
 describe("3-hero service (http) integration testing:", () => {
-    
-    it("getHeroes function: send request and receive response successfully", () => { })
-    it("updateHero function: send request and receive response successfully", () => { })
-})
+    let httpTesting: HttpTestingController;
+    let service: HeroServiceForLab;
+    let heroesUrl = 'http://localhost:3000/heroes'; //URL to web api
+
+    beforeEach(() => {
+        // 1
+        TestBed.configureTestingModule({
+            providers: [
+                // ... other test providers
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                HeroServiceForLab,
+            ]
+        });
+        // 2
+        httpTesting = TestBed.inject(HttpTestingController);
+        // 3
+        service = TestBed.inject(HeroServiceForLab);
+    });
+    it("getHeroes function: send request and receive response successfully", () => {
+        const mockHeroes: Hero[] = [
+            { id: 1, name: 'Hero 1', strength: 10 },
+            { id: 2, name: 'Hero 2', strength: 15 }
+        ];
+
+        service.getHeroes().subscribe({
+            next: (heroes) => {
+                expect(heroes.length).toBe(2);
+                expect(heroes).toEqual(mockHeroes);
+            }
+        });
+
+        // check conection and method
+        let req = httpTesting.expectOne(heroesUrl);
+        expect(req.request.method).toBe('GET');
+
+        req.flush(mockHeroes);
+    });
+
+    it("updateHero function: send request and receive response successfully", () => {
+        const updatedHero: Hero = { id: 1, name: 'Updated Hero', strength: 20 };
+
+        service.updateHero(updatedHero).subscribe({
+            next: (response) => {
+                expect(response).toEqual(updatedHero);
+            }
+        });
+        // check conection and method
+        let req = httpTesting.expectOne(heroesUrl);
+        expect(req.request.method).toBe('PUT');
+        expect(req.request.body).toEqual(updatedHero);
+        // moke res
+        req.flush(updatedHero);
+    });
+});
--- src/app/lab/messages/messages.lab.component.spec.ts
+++ src/app/lab/messages/messages.lab.component.spec.ts
@@ -1,10 +1,39 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MessagesComponentForLab } from './messages.lab.component';
+import { By } from '@angular/platform-browser';
 
 
 describe("2-message component integration testing:", () => {
-   
-   
+    let fixture: ComponentFixture<MessagesComponentForLab>, component: MessagesComponentForLab;
+    beforeEach(() => {
+        // 1
+        TestBed.configureTestingModule({ imports: [MessagesComponentForLab] })
+        // 2
+        fixture = TestBed.createComponent(MessagesComponentForLab);
+        component = fixture.componentInstance;
+    });
+
     it("expect component template to be empty", () => {
         //Note: there is @if"messageService.messages.length" in line 1 in template
+        expect(component).toBeTruthy();
+        const containerElement = fixture.debugElement.query(By.css('#container'));
+        expect(containerElement).toBeNull();
     })
-    it("then expect div.msg to have the messages after setting it", () => {})
+    it("then expect div.msg to have the messages after setting it", () => {
+        component.messageService.messages = [
+            { id: 1, message: "ITI .net" },
+            { id: 2, message: "ITI Mearn" }
+        ];
+
+        // update template
+        fixture.detectChanges();
+
+        // access msg duv
+        const messageDivs = fixture.debugElement.queryAll(By.css('div.msg'));
+        //check the divs linght tobe 2
+        expect(messageDivs.length).toBe(2);
+
+        expect(messageDivs[0].nativeElement.textContent).toContain("ITI .net");
+        expect(messageDivs[1].nativeElement.textContent).toContain("ITI Mearn");
+    })
 })
